7ba609c7e07665062dc820c4e28ed9f181042007
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-04-18 16:05:43 +0200

    n

--- PHP/account.php
+++ PHP/account.php
@@ -6,31 +6,23 @@ include_once('db.php');
 if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
     $editUserSql = '
         UPDATE users
-        SET firstname = :firstname,
-            lastname = :lastname,
-            email = :email,
-            gender = :gender
-        WHERE id = :id
+        SET email = :email,
+        WHERE pseudo = :pseudo
     ';
 
     $preparedEditUser = $dbh->prepare($editUserSql);
     $preparedEditUser->execute([
-        'firstname' => $_POST['firstname'],
-        'lastname' => $_POST['lastname'],
         'email' => $_POST['email'],
-        'gender' => $_POST['gender'],
-        'id' => $_SESSION['userId']
+        'pseudo' => $_SESSION['pseudo']
     ]);
-    $_SESSION['firstname'] = $_POST['firstname'];
-    $_SESSION['lastname'] = $_POST['lastname'];
 }
 
 
-$getUser = "SELECT id, firstname, lastname, email, gender FROM users WHERE id = :id";
+$getUser = "SELECT pseudo, email FROM users WHERE pseudo = :pseudo";
 
 $preparedGetUser = $dbh->prepare($getUser);
 $preparedGetUser->execute([
-        'id' => $_SESSION['userId']
+        'pseudo' => $_SESSION['pseudo']
 ]);
 
 $user = $preparedGetUser->fetch();
@@ -56,58 +48,15 @@ $user = $preparedGetUser->fetch();
         </nav>
     </header>
 
-    <h1>Bienvenue <?php echo $_SESSION['firstname'] . ' ' . $_SESSION['lastname'] ?></h1>
+    <h1>Bienvenue <?php echo $_SESSION['pseudo']?></h1>
 
     <form action="" method="POST">
 
-        <div>
-            <label for="firstname">Prenom</label>
-            <input
-                    id="firstname"
-                    type="text"
-                    name="firstname"
-                    placeholder="Prenom"
-                    value="<?php echo $user['firstname'] ?>"
-            >
-        </div>
-        <!--  NOM  -->
-        <div>
-            <label for="lastname">Nom</label>
-            <input id="lastname" type="text" name="lastname" value="<?php echo $user['lastname'] ?>">
-        </div>
         <!--  EMAIL  -->
         <div>
             <label for="email">Email</label>
             <input id="email" type="email" name="email" value="<?php echo $user['email'] ?>">
         </div>
-        <!--  GENRE (RADIO button)  -->
-        <div>
-            <label for="man">Homme</label>
-            <input
-                    id="man"
-                    type="radio"
-                    name="gender"
-                    value="man"
-                <?php echo $user['gender'] === "man" ? 'checked' : '' ?>
-            >
-
-            <label for="woman">Femme</label>
-            <input
-                    id="woman"
-                    type="radio"
-                    name="gender"
-                    value="woman"
-                <?php echo $user['gender'] === "woman" ? 'checked' : '' ?>
-            >
-
-            <label for="other">Autre</label>
-            <input
-                    id="other"
-                    type="radio"
-                    name="gender"
-                    value="other"
-                <?php echo $user['gender'] === "other" ? 'checked' : '' ?>
-            >
         </div>
 
         <input type="submit" value="Modifier">
