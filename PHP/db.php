2eb5bf342758526aacd8793c5271fa6f937dccae
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-04-18 20:02:59 +0200

    n

--- PHP/db.php
+++ PHP/db.php
@@ -1,10 +1,9 @@
 <?php
 $user = 'abdou';
 $password = 'tn3bbjTDe5UQ';
-$options = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
 
 try {
-    $dbh = new PDO('mysql:host=http://54.37.68.230;dbname=projet', $user, $password, $options);
+    $dbh = new PDO('mysql:host:54.37.68.230;dbname=projet', $user, $password, $options);
 } catch (PDOException $e) {
     var_dump($e);
 }
