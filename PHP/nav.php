67bbcc31615c7eee38f02a84a568d2dfa01311db
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-05-29 21:16:54 +0200

    n

--- PHP/nav.php
+++ PHP/nav.php
@@ -4,6 +4,14 @@ session_start();
 <header>
     <a href="index.php"><img src="ASSET/CARDBINDEX V4.png" alt="LOGO"></a>
     <a href="recherche/recherche.html">Rechercher une Carte</a>
-    <a class="Connexion" href="PHP/loginForm.php">Se Connecter</a>
-    <a id="param" href="PHP/compte.php"><img src="ASSET/PARAMETRE.png"></a>
+    <?php
+    if(isset($_SESSION['username'])) {
+        // Si l'utilisateur est connecté :
+    echo "<a id="param" href="PHP/compte.php"><img src="ASSET/PARAMETRE.png"></a>";
+    } else {
+        // Si l'utilisateur n'est pas connecté :
+    echo "<a class="Connexion" href="PHP/loginForm.php">Se Connecter</a>";
+    }
+
+    ?>
 </header>
