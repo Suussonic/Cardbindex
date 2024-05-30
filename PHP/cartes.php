a03c1113f010aa490bdd08adba666a4b7251ad75
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-05-30 02:17:16 +0200

    n

--- PHP/cartes.php
+++ PHP/cartes.php
@@ -26,7 +26,7 @@ $userCardsJson = json_encode($userCards);
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="../CSS/delete.css">
+    <link rel="stylesheet" href="../CSS/cartes.css">
     <title>Mes cartes</title>
 </head>
 <body>
