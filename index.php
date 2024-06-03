5feadc83cb5b35b7bbdb0086d8416077b4fc8ecb
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-06-03 19:29:03 +0200

    n

--- index.php
+++ index.php
@@ -9,8 +9,8 @@ session_start();
     <link rel="shortcut icon" href="ASSET/CARDBINDEX V5.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-    <link rel="stylesheet" href="CSS/Index.css">
     <?php include 'PHP/theme.php'; ?>
+    <link rel="stylesheet" href="CSS/Index.css">
     <title>Accueil</title>
 </head>
 <body>
