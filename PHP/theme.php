650a95801aefe550a25ee79e04ae19c7f9ca9695
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-06-03 19:33:12 +0200

    n

--- PHP/theme.php
+++ PHP/theme.php
@@ -4,14 +4,14 @@ include_once('db.php');
 
 if (isset($_SESSION['firstname'])) {
     // Si l'utilisateur est connecté
-    $userId = $_SESSION['user_id']; // Assurez-vous que l'ID de l'utilisateur est stocké dans la session
+    $userId = $_SESSION['firstnamee']; // Assurez-vous que l'ID de l'utilisateur est stocké dans la session
 
     // Préparez une requête pour vérifier la valeur du captcha
-    $stmt = $pdo->prepare('SELECT captcha FROM users WHERE id = :id');
+    $stmt = $pdo->prepare('SELECT theme FROM users WHERE id = :id');
     $stmt->execute(['id' => $userId]);
     $user = $stmt->fetch();
 
-    if ($user && $user['captcha'] == 1) {
+    if ($user && $user['theme'] == 0) {
         echo '<link rel="stylesheet" href="../CSS/white.css">';
     } else {
         echo '<link rel="stylesheet" href="../CSS/black.css">';
