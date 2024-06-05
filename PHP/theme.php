57cb8d420f64117c948f332d0bb56b727e7d8eca
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-06-05 22:43:48 +0200

    h

--- PHP/theme.php
+++ PHP/theme.php
@@ -3,17 +3,21 @@ global $dbh;
 session_start();
 include_once('db.php');
 
-if (isset($_SESSION['firstname'])) { // Utiliser l'ID de l'utilisateur stocké dans la session
+if (isset($_SESSION['id'])) { // Utiliser l'ID de l'utilisateur stocké dans la session
     $userId = $_SESSION['id']; // Assurez-vous que l'ID de l'utilisateur est bien stocké dans la session
 
     // Préparez une requête pour obtenir le thème de l'utilisateur
+    $getUser = "SELECT theme FROM users WHERE id = :id";
     
     $preparedGetUser = $dbh->prepare($getUser);
     $preparedGetUser->execute([
-            'id' => $_SESSION['userId']
+        'id' => $userId
     ]);
 
-    if ($preparedGetUser == 1) { // Vérifiez la valeur du thème (0 ou 1)
+    // Récupérez le résultat de la requête
+    $result = $preparedGetUser->fetch(PDO::FETCH_ASSOC);
+
+    if ($result && $result['theme'] == 1) { // Vérifiez la valeur du thème (0 ou 1)
         echo '<link rel="stylesheet" href="../CSS/white.css">';
     } else {
         echo '<link rel="stylesheet" href="../CSS/black.css">';
