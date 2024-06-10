61bd8931e93cf1b8d60cac32dbfb17e5e769dc3d
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-06-10 09:57:42 +0200

    b

--- PHP/theme.php
+++ PHP/theme.php
@@ -2,35 +2,22 @@
 session_start();
 include_once('db.php');
 
-// Assurez-vous que la connexion à la base de données fonctionne
-global $dbh;
+if (isset($_SESSION['firstname'])) {
+    // Si l'utilisateur est connecté
+    $userId = $_SESSION['firstname']; // Assurez-vous que l'ID de l'utilisateur est stocké dans la session
 
-if (isset($_SESSION['id'])) { // Utiliser l'ID de l'utilisateur stocké dans la session
-    $userId = $_SESSION['id']; // Assurez-vous que l'ID de l'utilisateur est bien stocké dans la session
+    // Préparez une requête pour vérifier la valeur du captcha
+    $stmt = $pdo->prepare('SELECT theme FROM users WHERE id = :id');
+    $stmt->execute(['id' => $userId]);
+    $user = $stmt->fetch();
 
-    // Préparez une requête pour obtenir le thème de l'utilisateur
-    $getUser = "SELECT theme FROM users WHERE id = :id";
-    
-    $preparedGetUser = $dbh->prepare($getUser);
-    $preparedGetUser->execute([
-        'id' => $userId
-    ]);
-
-    // Récupérez le résultat de la requête
-    $result = $preparedGetUser->fetch(PDO::FETCH_ASSOC);
-
-    if ($result) { // Si un résultat est trouvé
-        if ($result['theme'] == 1) { // Vérifiez la valeur du thème (1 pour blanc)
-            echo '<link rel="stylesheet" href="../CSS/white.css">';
-        } else { // Sinon, utilisez le thème noir
-            echo '<link rel="stylesheet" href="../CSS/black.css">';
-        }
+    if ($user && $user['theme'] == 0) {
+        echo '<link rel="stylesheet" href="../CSS/white.css">';
     } else {
-        // Si aucun résultat trouvé, utilisez le thème par défaut (noir)
         echo '<link rel="stylesheet" href="../CSS/black.css">';
     }
 } else {
-    // Si l'utilisateur n'est pas connecté, utilisez le thème par défaut (noir)
+    // Si l'utilisateur n'est pas connecté
     echo '<link rel="stylesheet" href="../CSS/black.css">';
 }
 ?>
