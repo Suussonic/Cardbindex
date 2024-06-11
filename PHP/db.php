b395931e19adb1a61275893be360012973d197a4
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-06-11 15:30:54 +0200

    ff

--- PHP/db.php
+++ PHP/db.php
@@ -5,7 +5,7 @@ $password = 'tn3bbjTDe5UQ';
 $options = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
 
 try {
-    $dbh = new PDO('mysql:host=localhost;dbname=pa', $user, $password, $options);
+    $dbh = new PDO('mysql:host=localhost;dbname=promo4', $user, $password, $options);
 } catch (PDOException $e) {
     var_dump($e);
 }
