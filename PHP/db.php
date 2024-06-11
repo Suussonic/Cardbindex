d4b1005913c7081fe12035b71ffaac583fd5df24
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-06-11 14:33:32 +0200

    c

--- PHP/db.php
+++ PHP/db.php
@@ -5,8 +5,11 @@ $password = 'tn3bbjTDe5UQ';
 $options = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
 
 try {
-    // $dbh = new PDO('mysql:host=localhost;dbname=promo4', $user, $password, $options);
-    $dbh = new PDO('mysql:host=mariadb;dbname=promo4', $user, $password, $options);
+    $dbh = new PDO('mysql:host=localhost;dbname=pa', $user, $password, $options);
 } catch (PDOException $e) {
     var_dump($e);
 }
+
+//$userQuery = $dbh->query("SELECT * FROM users");
+//$allUsers = $userQuery->fetchAll();
+//var_dump($allUsers);
