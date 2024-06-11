150b4328cd4948abd1874e58a1e6f881174e7a51
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-06-11 14:41:39 +0200

    v

--- PHP/theme.php
+++ PHP/theme.php
@@ -1,29 +1,23 @@
 <?php
+session_start();
+include_once('db.php');
 
-if (isset($_SESSION)) { 
-    $userId = $_SESSION['userId'];
+if (isset($_SESSION['firstname'])) {
+    // Si l'utilisateur est connecté
+    $userId = $_SESSION['firstname']; // Assurez-vous que l'ID de l'utilisateur est stocké dans la session
 
-    $getUser = "SELECT theme FROM users WHERE id = :id";
-    
-    $preparedGetUser = $dbh->prepare($getUser);
-    $preparedGetUser->execute([
-        'id' => $userId
-    ]);
+    // Préparez une requête pour vérifier la valeur du captcha
+    $stmt = $pdo->prepare('SELECT theme FROM users WHERE id = :id');
+    $stmt->execute(['id' => $userId]);
+    $user = $stmt->fetch();
 
-    $result = $preparedGetUser->fetch(PDO::FETCH_ASSOC);
-
-    if ($result) { 
-        if ($result['theme'] == 1) {
-            echo '<link rel="stylesheet" href="../CSS/black.css">';
-        } elseif ($result['theme'] == 0) { 
-            echo '<link rel="stylesheet" href="../CSS/white.css">';
-        } else {
-            echo '<link rel="stylesheet" href="../CSS/default.css">';
-        }
+    if ($user && $user['theme'] == 0) {
+        echo '<link rel="stylesheet" href="../CSS/white.css">';
     } else {
-        echo '<link rel="stylesheet" href="../CSS/default.css">';
+        echo '<link rel="stylesheet" href="../CSS/black.css">';
     }
 } else {
-    echo '<link rel="stylesheet" href="../CSS/default.css">';
+    // Si l'utilisateur n'est pas connecté
+    echo '<link rel="stylesheet" href="../CSS/black.css">';
 }
 ?>
