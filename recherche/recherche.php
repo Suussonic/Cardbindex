b4d638df4b27c24876af6475a9ff1affb0df538b
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-06-25 11:06:30 +0200

    f

--- recherche/recherche.php
+++ recherche/recherche.php
@@ -2,9 +2,6 @@
 global $dbh;
 session_start();
 require_once('../PHP/db.php');
-?>
-
-<?php
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Code de mise à jour de l'utilisateur...
@@ -20,20 +17,21 @@ $preparedGetUser->execute([
 
 $user = $preparedGetUser->fetch();
 
-// Récupération du prénom de l'utilisateur
-$firstname = $user['firstname'];
+// Récupération de l'ID de l'utilisateur
+$userId = $user['id'];
 
-// Utilisation du prénom récupéré
-echo "Le prénom de l'utilisateur est : $firstname";
+// Utilisation de l'ID récupéré
+echo "L'ID de l'utilisateur est : $userId";
 
 $cardId = $_POST["cardId"];
 $insertUser = "
-INSERT INTO classeur (firstname, id_carte)
-VALUES (:firstname, :cardId)
+INSERT INTO classeur (id_user, id_carte)
+VALUES (:userId, :cardId)
 ";
 
 $preparedQuery = $dbh->prepare($insertUser);
 $preparedQuery->execute([
     'cardId' => $cardId,
-    'firstname' => $firstname,
+    'userId' => $userId,
 ]);
+?>
