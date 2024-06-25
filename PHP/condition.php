0a2e755e7b0d35587ed9afbd3c7d5edd48cd1de7
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-06-25 10:22:41 +0200

    f

---
+++ PHP/condition.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="sUTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../ASSET/CARDBINDEX V5.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="stylesheet" href="../CSS/condition.css">
+    <title>Condition d'utilisation</title>
+</head>
+
+<body>
+    <section>
+        <P>
+            CONDITION D'UTILISATION A REMPLIR
+        </p>
+    </section>
+    <footer>
+        <div id="Credit">
+            <p>© 2024 Pokémon. © 1995–2024 Nintendo/Creatures Inc./GAME FREAK Inc. est une marque déposée par Nintendo</p>
+            <p>© 2024, CardBinDex. Les autres marques, images ou noms de produit appartiennent à leurs propriétaires respectifs.</p>
+        </div>
+        <div id="Lien">
+            <h2>Nous Contacter</h2>
+            <h2>projet.annuel3tan@gmail.com</h2>
+            <h2>Nos réseaux :</h2>
+            <a href="https://twitter.com/cardbindex" target="_blank"><img src="./ASSET/X.png" alt="TWITTER" width="24px" height="24px"></a>
+            <a href="https://twitter.com/cardbindex" target="_blank"><img src="./ASSET/DISCORD.png" alt="DISCORD" width="24px" height="24px"></a>
+            <a href="https://twitter.com/cardbindex" target="_blank"><img src="./ASSET/INSTAGRAM.png" alt="INSTAGRAM" width="24px" height="24px"></a>
+            <a href="https://github.com/Suussonic/CardBinDex" target="_blank"><img src="./ASSET/GITHUB.png" alt="GITHUB" width="24px" height="24px"></a>
+        </div>
+    </footer>
+</body>
+
+</html>
