b7f6caeac42f3c564748acd69853a1e73de1b95a
Titouan <titouan.melchio@gmail.com>
2024-07-03 17:00:49 +0200

    lisjr

--- PHP/nav.php
+++ PHP/nav.php
@@ -3,7 +3,7 @@
     <a href="../recherche/recherche.html">Rechercher</a>
     <?php
     if (isset($_SESSION['firstname'])) {
-        echo '<a id="param" href="PHP/parametre.php"><img src="/ASSET/PARAMETRE.png"></a>';  
+        echo '<a id="param" href="BACK/parametre.php"><img src="/ASSET/PARAMETRE.png"></a>';  
     } else {
         echo '<a class="Connexion" href="PHP/loginForm.php">Se Connecter</a>';
     }
