7c9276f83b34be9cd98b8411ead329c8ea182d7c
Titouan <titouan.melchio@gmail.com>
2024-07-04 12:16:35 +0200

    mdp complex

--- PHP/createAccount.php
+++ PHP/createAccount.php
@@ -4,6 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 global $dbh;
 require_once('db.php');
+include('../BACK/verifmdp.php');
 
 if (isset($_POST['captcha_input'])) {
     $user_answer = $_POST['captcha_input'];
@@ -14,23 +15,27 @@ if (isset($_POST['captcha_input'])) {
         $pass = $_POST['password'];
         $gender = $_POST['gender'];
 
-        $passHash = password_hash($pass, PASSWORD_BCRYPT);
+        if (verifierMotDePasse($pass)) {
+            $passHash = password_hash($pass, PASSWORD_BCRYPT);
 
-        $insertUser = "
+            $insertUser = "
             INSERT INTO users (firstname, lastname, email, password, gender)
             VALUES (:firstname, :lastname, :email, :password, :gender)
             ";
 
-        $preparedQuery = $dbh->prepare($insertUser);
-        $preparedQuery->execute([
-            'firstname' => $firstname,
-            'lastname' => $lastname,
-            'email' => $email,
-            'password' => $passHash,
-            'gender' => $gender,
-        ]);
-        header("Location: /");
-        exit;
+            $preparedQuery = $dbh->prepare($insertUser);
+            $preparedQuery->execute([
+                'firstname' => $firstname,
+                'lastname' => $lastname,
+                'email' => $email,
+                'password' => $passHash,
+                'gender' => $gender,
+            ]);
+            header("Location: /");
+            exit;
+        } else {
+            echo "Votre mot de passe doit posséder un minimum de 8 caractères, dont une majuscule, une minuscule, un caractère spécial et un chiffre.";
+        }
     } else {
         echo "réponse incorrecte. Veuillez réessayer";
         header("Location: form.php");
@@ -39,5 +44,3 @@ if (isset($_POST['captcha_input'])) {
 } else {
     echo "Veuillez répondre au captcha.";
 }
-
-?>
