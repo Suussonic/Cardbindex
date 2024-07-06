a0debe1cea166747c40aa7cdb5ab88dd6fdc7719
Titouan <titouan.melchio@gmail.com>
2024-07-06 17:58:46 +0200

    yge

--- PHP/graph.php
+++ PHP/graph.php
@@ -8,7 +8,29 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-  <?php include '../PHP/db.php'; ?>
+  <?php 
+  include '../PHP/db.php';
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+  
+  header('Content-Type: application/json'); // Assurer que le type de contenu est JSON
+  
+  include('../PHP/db.php');
+  
+  $sql = "SELECT * FROM logs";
+  $result = $conn->query($sql);
+  
+  $logs = array();
+  if ($result->num_rows > 0) {
+      while($row = $result->fetch_assoc()) {
+          $logs[] = $row;
+      }
+  }
+  echo json_encode($logs);
+  
+  $conn->close();
+  ?>
 
   <div class="aws-container">
     <div class="aws-content">
