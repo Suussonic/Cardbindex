3af94899a33df4ef7683db669be907015591aec3
Titouan <titouan.melchio@gmail.com>
2024-07-06 17:48:00 +0200

    ?.?.?

--- PHP/graph.php
+++ PHP/graph.php
@@ -8,7 +8,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-  <?php include 'db.php'; ?>
+  <?php include '../PHP/db.php'; ?>
 
   <div class="aws-container">
     <div class="aws-content">
