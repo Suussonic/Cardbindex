ede48f5daff994246e46a83eaca91ba428a37133
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-07-07 14:15:01 +0200

    f

--- BACK/parametre.php
+++ BACK/parametre.php
@@ -18,36 +18,15 @@ include_once('db.php');
                 <li><a href="#page1">Informations du compte</a></li>
                 <li><a href="#page2">Profil utilisateur</a></li>       
                 
-                <?php
-                session_start();
-                // Inclure le fichier de connexion à la base de données
-                include_once('db.php');
-                if (isset($_SESSION['userId'])) { 
-                    $userId = $_SESSION['userId'];
-
-                    $getUser = "SELECT roole FROM users WHERE id = :id";
-                    
-                    $preparedGetUser = $dbh->prepare($getUser);
-                    $preparedGetUser->execute([
-                        'id' => $userId
-                    ]);
-
-                    $result = $preparedGetUser->fetch(PDO::FETCH_ASSOC);
-
-                    if ($result) { 
-                        if ($result['role'] == 'admin') {
-                            echo '<div class="dropdown">
-                                <span>Administration</span>
-                                <div class="dropdown-content">
-                                    <a href="#page3">- Liste utilisateur</a><br>
-                                    <a href="#page4">- Captcha</a><br>
-                                    <a href="#page5">- Logs</a><br>
-                                    <a href="#page6">- Newsletter</a><br>
-                                </div></div>"';
-                        }
-                    }
-                }
-            ?>     
+                <div class="dropdown">
+                    <span>Administration</span>
+                    <div class="dropdown-content">
+                        <a href="#page3">- Liste utilisateur</a><br>
+                        <a href="#page4">- Captcha</a><br>
+                        <a href="#page5">- Logs</a><br>
+                        <a href="#page6">- Newsletter</a><br>
+                    </div>
+                </div>
             </ul>
         </div>
 
