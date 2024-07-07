7fa87eecc5f9ebb9f3045ddacda1496f17dab3c1
Titouan <titouan.melchio@gmail.com>
2024-07-07 21:39:31 +0200

    s

--- BACK/display_captcha.php
+++ BACK/display_captcha.php
@@ -7,6 +7,10 @@ if (isset($_POST['delete_id'])) {
     $delete_sql = "DELETE FROM captcha WHERE id = :id";
     $stmt = $dbh->prepare($delete_sql);
     $stmt->execute([':id' => $delete_id]);
+
+    // Redirect to the same page to prevent form resubmission
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit;
 }
 
 // Définir la requête SQL pour récupérer les données de la table captcha
