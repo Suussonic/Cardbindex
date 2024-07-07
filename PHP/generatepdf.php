22794640563cca736f9dc15e28742a16c1efd6cf
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-07-07 23:31:17 +0200

    f

---
+++ PHP/generatepdf.php
@@ -0,0 +1,5 @@
+<?php
+ 
+require_once('../fpdf186/fpdf.php');
+
+?>
---
+++ PHP/rechercheusers.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    
+    <link rel="shortcut icon" href="../ASSET/CARDBINDEX V5.png" type="image/x-icon">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="../recherche/recherche.css">
+    <title>Rechercher un utilisateur</title>
+</head>
+
+<body>
+    <header>
+        <a href="../index.php"><img src="../ASSET/CARDBINDEX V4.png" alt="LOGO"></a>
+        <a href="../recherche/recherche.html">Rechercher</a>
+    </header>
+    <h1>Rechercher un utilisateur !</h1>
+    <form>
+        <div class="form-row align-items-center">
+            <div class="col-auto">
+                <label class="sr-only" for="search">Utilisateur</label>
+                <input type="text" class="form-control mb-2" id="search">
+            </div>
+            <div class="col-auto">
+                <button type="submit" class="btn btn-primary mb-2" id="submit-button">Rechercher</button>
+            </div>
+        </div>
+    </form>
+
+</html>
