fc6c27a3309f674c7b92a93af85a112677a914b4
Suussonic <150890672+Suussonic@users.noreply.github.com>
2024-07-07 14:03:16 +0200

    ff

---
+++ BACK/admin.php
@@ -0,0 +1,27 @@
+<?php
+    if (isset($_SESSION['userId'])) { 
+        $userId = $_SESSION['userId'];
+
+        $getUser = "SELECT role FROM users WHERE id = :id";
+        
+        $preparedGetUser = $dbh->prepare($getUser);
+        $preparedGetUser->execute([
+            'id' => $userId
+        ]);
+
+        $result = $preparedGetUser->fetch(PDO::FETCH_ASSOC);
+
+        if ($result) { 
+            if ($result['role'] == 'admin') {
+                <div class="dropdown">
+                    <span>Administration</span>
+                    <div class="dropdown-content">
+                        <a href="#page3">- Liste utilisateur</a><br>
+                        <a href="#page4">- Captcha</a><br>
+                        <a href="#page5">- Logs</a><br>
+                        <a href="#page6">- Newsletter</a><br>
+                    </div>
+                </div>
+            }
+        }
+?>
--- BACK/parametre.php
+++ BACK/parametre.php
@@ -11,36 +11,8 @@
         <div class="sidebar">
             <ul>
                 <li><a href="#page1">Informations du compte</a></li>
-                <li><a href="#page2">Profil utilisateur</a></li>
-                
-                <?php
-                if (isset($_SESSION['userId'])) { 
-                    $userId = $_SESSION['userId'];
-
-                    $getUser = "SELECT role FROM users WHERE id = :id";
-                    
-                    $preparedGetUser = $dbh->prepare($getUser);
-                    $preparedGetUser->execute([
-                        'id' => $userId
-                    ]);
-
-                    $result = $preparedGetUser->fetch(PDO::FETCH_ASSOC);
-
-                    if ($result) { 
-                        if ($result['role'] == 'admin') {
-                            <div class="dropdown">
-                                <span>Administration</span>
-                                <div class="dropdown-content">
-                                    <a href="#page3">- Liste utilisateur</a><br>
-                                    <a href="#page4">- Captcha</a><br>
-                                    <a href="#page5">- Logs</a><br>
-                                    <a href="#page6">- Newsletter</a><br>
-                                </div>
-                            </div>
-                        }
-                    }
-                ?>
-                
+                <li><a href="#page2">Profil utilisateur</a></li>                
+                <?php include 'admin.php'; ?>
             </ul>
         </div>
 
