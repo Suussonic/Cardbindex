4e8f9820a8c1ee9022226966526d36dfc999545e
Titouan <titouan.melchio@gmail.com>
2024-07-07 19:28:32 +0200

    fffffd

--- BACK/parametre.php
+++ BACK/parametre.php
@@ -139,7 +139,7 @@ include_once('db.php');
             </div>
             <div id="page4" class="page">
                 <h1>Captcha</h1>
-                <?php include 'display_captcha.php'; ?>  
+                
             </div>
             <div id="page5" class="page">
                 <h1>Logs</h1>
